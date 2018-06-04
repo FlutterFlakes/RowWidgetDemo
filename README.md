74c61e3b8358947d8e23deddf0572886741cf6b8
Dhrumil Shah <dhuma1981@gmail.com>
2018-06-04 14:50:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# row_widget_demo
+
+Row Widget Demo
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: "Row Demo",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Row Demo"),
+      ),
+      body: RowWidgetDemo(),
+    ),
+
+  )
+);
+
+class RowWidgetDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
